349b3baa9a4be3e12fdc201680c85960f7f3a273
Alexander Gavrilyuk <gavrilyuk.alex@gmail.com>
2019-08-22 09:48:17 +0300

    Innitial commit

---
+++ src/main/java/com/cursor/hw_15_spring_security/Hw15SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cursor.hw_15_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw15SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw15SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursor/hw_15_spring_security/Hw15SpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cursor.hw_15_spring_security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hw15SpringSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
