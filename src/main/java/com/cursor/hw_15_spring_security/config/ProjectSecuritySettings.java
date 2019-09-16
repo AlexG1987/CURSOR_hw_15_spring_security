e2f37fe80c3b303d894f4db463d8e607b4ce8c37
Alexander Gavrilyuk <gavrilyuk.alex@gmail.com>
2019-09-16 13:17:27 +0300

    Simplifycated some code

--- src/main/java/com/cursor/hw_15_spring_security/config/ProjectSecuritySettings.java
+++ src/main/java/com/cursor/hw_15_spring_security/config/ProjectSecuritySettings.java
@@ -1,60 +1,30 @@
 package com.cursor.hw_15_spring_security.config;
 
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true)
-@AllArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class ProjectSecuritySettings extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("userServiceImpl")
-    @Autowired
-    private UserDetailsService userService;
-
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userService);
-        return provider;
-    }
-
-    private InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryConfigure() {
-        return new InMemoryUserDetailsManagerConfigurer<>();
-    }
-
-    public void configureGlobal(AuthenticationManagerBuilder builder,
-                                AuthenticationProvider provider) throws Exception {
-        inMemoryConfigure()
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
                 .withUser("admin")
                 .password("admin")
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
                 .password("user")
-                .roles("USER")
-                .and()
-                .configure(builder);
-        builder.authenticationProvider(provider);
+                .roles("USER");
     }
 
 
@@ -64,12 +34,14 @@ public class ProjectSecuritySettings extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/**").permitAll()
                 .antMatchers("/user/**").hasRole("USER")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
                 .csrf().disable()
-                .formLogin().disable();
+                .formLogin().disable()
+                .headers()
+                .frameOptions()
+                .sameOrigin();
     }
 
     @Bean
--- src/main/java/com/cursor/hw_15_spring_security/model/Role.java
+++ src/main/java/com/cursor/hw_15_spring_security/model/Role.java
@@ -2,5 +2,4 @@ package com.cursor.hw_15_spring_security.model;
 
 public enum Role {
     USER, ADMIN
-
 }
